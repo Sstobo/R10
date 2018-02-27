a780369b3119f8be22df79a083aaf8c266f14eee
Sean <sean@iMac.hitronhub.home>
2018-02-27 06:50:44 -0800

    About container fetch component added

--- js/scenes/About/About.js
+++ js/scenes/About/About.js
@@ -1,15 +1,43 @@
-//import liraries
+
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { View, Text } from "react-native";
+import {
+   View, 
+   Text,
+   SectionList
+   } 
+   from "react-native";
 
-import { styles } from "./styles";
+// import { styles } from "./styles";
 
-// create a component
+
 const About = () => (
+  
   <View style={styles.container}>
-    <Text>ABOUT SCENE</Text>
+
+    <Text sty> R10 </Text>
+    <Text>R10 is a conference that focuses on just abouut any toipic related to dev.</Text>
+    <Text>Date & Venue</Text>
+    <Text>The R10 conference will take place on Tuesday, June 27, 2017 in Vancouver, BC</Text>
+    <Text>Code of Conduct</Text>
+
+    <SectionList 
+    sections={this.props.data}
+    renderItem = {({item}) => <Text> {item} </Text>}
+    renderSectionHeader={({ section}) => <Text> {section.title}</Text>}
+    keyExtractor={item => item}
+    />
+
   </View>
 );
+
+
+styles = StyleSheet.create({
+
+
+
+    
+});
+
 
 export default About;
--- js/scenes/About/AboutContainer.js
+++ js/scenes/About/AboutContainer.js
@@ -4,18 +4,39 @@ import React, { Component } from "react";
 // import { View, Text, StyleSheet } from 'react-native';
 
 import About from "./About";
+// this file creates and shares the props
 
 // create a component
+
+
 class AboutContainer extends Component {
   constructor() {
     super();
-    this.state = { Loading: true };
+    this.state = { 
+      loading: true,
+      data = []
+    
+    };
 
+    componentDidMount(){
+      let endpoint = "https://r10app-95fea.firebaseio.com/code_of_conduct.json";
+      fetch(endpoint)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ data });
+      })
+      .catch(err => console.log(`Error fetching JSON: ${error}`))
+    }
     // component DidMOunt //Fetch data  Loading: false
   }
   render() {
-    return <About />;
+
+    return 
+    {this.state.data && (
+    <About data = {this.state.data}/>
+    )
   }
+}
 }
 
 //make this component available to the app
