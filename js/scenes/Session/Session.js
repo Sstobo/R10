55a53f0ee7255374979ba388ed3e67e3f0befe50
Sstobo <seanstobo@gmail.com>
2018-02-28 06:48:39 -0800

    Modules updated

--- js/scenes/Session/Session.js
+++ js/scenes/Session/Session.js
@@ -15,3 +15,30 @@ const Session = () => (
 export default Session;
 
 // use the helper function mandy shared
+// in helpers file 
+// use section list for sessions
+// helper shapes data for sessions
+
+
+<SectionList
+  sections={[
+    {title: 'ADP', data: ['Bob', 'Alice']},
+    {title: 'WDP', data: ['Anne', 'Mary', 'Joe']},
+  ]}
+  renderItem={({ item }) => <Text>{item}</Text>}
+  renderSectionHeader={({ section }) => <Text>{section.title}</Text>}
+/>
+// Helper to format format individual Firebase records
+export const formatDataObject = (data) => {
+  const dataObject = Object.getOwnPropertyNames(data).map(index => data[index]);
+  return dataObject[0];
+}
+
+// Helper to format Firebase data into section list data
+export const formatSessionData = (sessions) => {
+  return sessions.reduce((acc, curr) => {
+    const timeExists = acc.find(section => section.title === curr.start_time)
+    timeExists ? timeExists.data.push(curr) : acc.push({title: curr.start_time, data: [curr]});
+    return acc;
+  }, []).sort((a, b) => a.title - b.title);
+};
