a414a5bdfdfc06f6a56405cbf4005e7ad3eea5fd
Sstobo <seanstobo@gmail.com>
2018-03-04 17:10:19 -0800

    Sunday laptop sync

---
+++ js/scenes/Speaker/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	container: {
+		margin: 14,
+		marginTop: 24
+	},
+	title: {
+		color: 'black',
+		fontSize: 20,
+		marginBottom: 12,
+		fontFamily: 'Montserrat'
+	},
+	image: {
+		width: 120,
+		height: 120,
+		marginBottom: 24,
+		borderRadius: 60
+	},
+	description: {
+		fontSize: 16,
+		lineHeight: 24,
+		marginTop: 12,
+		marginBottom: 34,
+		fontFamily: 'Montserrat'
+	},
+	link: {
+		fontSize: 18,
+		color: 'purple',
+		marginBottom: 24,
+		fontFamily: 'Montserrat'
+	},
+	speaker: {
+		fontFamily: 'Montserrat',
+		fontSize: 20,
+		paddingTop: 8,
+		color: 'black'
+	}
+});
