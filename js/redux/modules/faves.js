e4d60fa5ed3172ba12e11bea04af6f780457b56e
Sstobo <seanstobo@gmail.com>
2018-03-05 07:26:59 -0800

    Syncing desktop

--- js/redux/modules/faves.js
+++ js/redux/modules/faves.js
@@ -1,38 +1,38 @@
-import { queryFaves } from "../../config/model";
+import { queryFaves } from '../../config/models';
 
-const GET_FAVES_LOADING = "GET_FAVES_LOADING";
-const GET_FAVES = "GET_FAVES";
-const GET_FAVES_ERROR = "GET_FAVES_ERROR";
+const GET_FAVES_LOADING = 'GET_FAVES_LOADING';
+const GET_FAVES = 'GET_FAVES';
+const GET_FAVES_ERROR = 'GET_FAVES_ERROR';
 
 const getFavesLoading = () => ({ type: GET_FAVES_LOADING });
 const getFaves = faves => ({
-  type: GET_FAVES,
-  payload: faves
+	type: GET_FAVES,
+	payload: faves
 });
 const getFavesError = error => ({
-  type: GET_FAVES_ERROR,
-  payload: error
+	type: GET_FAVES_ERROR,
+	payload: error
 });
 
 export const fetchFaves = () => dispatch => {
-  dispatch(getFavesLoading());
-  const faves = queryFaves();
-  console.log(faves);
-  dispatch(getFaves(faves));
+	dispatch(getFavesLoading());
+	const faves = queryFaves();
+	console.log('#############', faves);
+	dispatch(getFaves(faves));
 };
 
-export default (state = { loading: false, faves: {}, error: "" }, action) => {
-  switch (action.type) {
-    case GET_FAVES_LOADING: {
-      return { ...state, loading: true };
-    }
-    case GET_FAVES: {
-      return { ...state, loading: false, faves: action.payload };
-    }
-    case GET_FAVES_ERROR: {
-      return { ...state, loading: false, error: action.payload };
-    }
-    default:
-      return state;
-  }
+export default (state = { loading: false, faves: {}, error: '' }, action) => {
+	switch (action.type) {
+		case GET_FAVES_LOADING: {
+			return { ...state, loading: true };
+		}
+		case GET_FAVES: {
+			return { ...state, loading: false, faves: action.payload };
+		}
+		case GET_FAVES_ERROR: {
+			return { ...state, loading: false, error: action.payload };
+		}
+		default:
+			return state;
+	}
 };
--- js/scenes/Faves/FavesContainer.js
+++ js/scenes/Faves/FavesContainer.js
@@ -1,39 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import realm, { queryFaves } from '../../config/models';
+import { fetchFaves } from '../../redux/modules/faves';
+import { formatSessionData } from '../../redux/helpers/dataReshaper';
 
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import realm, { queryFaves } from "../../config/models";
-import { fetchSomeFaves } from "../../redux/modules/faves";
-import { formatSessionData } from "../../redux/helpers/dataReshaper";
-
-import Faves from "./Faves";
+import Faves from './Faves';
 
 class FavesContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  static route = {
-    navigationBar: {
-      title: "Faves"
-    }
-  };
+	static route = {
+		navigationBar: {
+			title: 'Faves'
+		}
+	};
 
-  componentDidMount() {
-    this.props.dispatch(fetchSomeFaves());
-  }
+	componentDidMount() {
+		this.props.dispatch(fetchFaves());
+	}
 
-  render() {
-    // console.log(this.props.faves);
-    // console.log(this.props.sessionData);
-    return (
-      <Faves faves={this.props.faves} sessionData={this.props.sessionData} />
-    );
-  }
+	render() {
+		console.log(this.props.faves);
+		console.log(this.props.sessionData);
+		return <Faves faves={this.props.faves} sessionData={this.props.sessionData} />;
+	}
 }
 
 const mapStateToProps = state => ({
-  faves: state.faves.faves,
-  sessionData: state.schedule.sessionData
+	faves: state.faves.faves,
+	sessionData: state.schedule.sessionData
 });
 
 export default connect(mapStateToProps)(FavesContainer);
