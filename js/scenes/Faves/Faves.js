0f40645ce94adc3cf55ae16b2e8b22d9456c8e8a
Sstobo <seanstobo@gmail.com>
2018-03-05 07:39:07 -0800

    Working on faves bug

--- js/scenes/Faves/Faves.js
+++ js/scenes/Faves/Faves.js
@@ -1,32 +1,23 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { styles } from "./styles";
+import React from 'react';
+
+import { styles } from './styles';
 import { formatUnixDate } from './../../redux/helpers/dataReshaper';
-import { SectionList, View, Text } from "react-native";
+import { SectionList, View, Text } from 'react-native';
 
-
-	const Faves = ({ data, loading, error, currentUID, style }) => {
-		if (error) {
-		  return <View />;
-		}
-		return (
-		  <SectionList
-			renderItem={({ item }) => (
-			  <ListItem item={item} currentUID={currentUID} style={style} />
-			)}
-			renderSectionHeader={({ section }) => (
-			  <Text style={style.header}>
-				{formatUnixDate(section.title)}
-			  </Text>
-			)}
+const Faves = ({ data, loading, error, currentUID, style }) => {
+	if (error) {
+		return <View />;
+	}
+	return (
+		<SectionList
+			renderItem={({ item }) => <ListItem item={item} currentUID={currentUID} style={style} />}
+			renderSectionHeader={({ section }) => <Text style={style.header}>{formatUnixDate(section.title)}</Text>}
 			sections={data}
 			keyExtractor={item => {
-			  return item.session_id;
+				return item.session_id;
 			}}
-		  />
-		);
-	  };
-
-
+		/>
+	);
+};
 
 export default Faves;
--- js/scenes/Faves/FavesContainer.js
+++ js/scenes/Faves/FavesContainer.js
@@ -1,52 +1,10 @@
-// import React, { Component } from "react";
-// import { connect } from "react-redux";
-// import realm, { queryFaves } from "../../config/models";
-// import { fetchSomeFaves } from "../../redux/modules/faves";
-// import { formatSessionData } from "../../redux/helpers/dataReshaper";
-
-// import Faves from "./Faves";
-
-// class FavesContainer extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   static route = {
-//     navigationBar: {
-//       title: "Faves"
-//     }
-//   };
-
-//   componentDidMount() {
-//     this.props.dispatch(fetchSomeFaves());
-//   }
-
-//   render() {
-//     // console.log(this.props.faves);
-//     // console.log(this.props.sessionData);
-//     return (
-//       <Faves faves={this.props.faves} sessionData={this.props.sessionData} />
-//     );
-//   }
-// }
-
-// const mapStateToProps = state => ({
-//   faves: state.faves.faves,
-//   sessionData: state.schedule.sessionData
-// });
-
-// export default connect(mapStateToProps)(FavesContainer);
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchFaves } from '../../redux/modules/faves';
-// import PropTypes from 'prop-types'
-import List from '../../components/List';
-import Gradient from '../../components/Gradient';
+import Faves from './Faves';
 import { fetchSchedule } from '../../redux/modules/schedule';
-import { formatSessionData } from '../../helpers/dataReshaper';
-import PropTypes from 'prop-types';
-import style from './style';
+import { formatSessionData } from '../../redux/helpers/dataReshaper';
+// import PropTypes from 'prop-types';
 
 class FavesContainer extends Component {
 	constructor(props) {
@@ -54,6 +12,7 @@ class FavesContainer extends Component {
 		this.state = {};
 	}
 	componentDidMount() {
+		console.log('FETCHING');
 		this.props.dispatch(fetchFaves());
 		this.props.dispatch(fetchSchedule());
 	}
@@ -75,15 +34,13 @@ class FavesContainer extends Component {
 
 	static route = {
 		navigationBar: {
-			title: 'Faves',
-			titleStyle: titleBarStyle,
-			renderBackground: () => <Gradient />
+			title: 'Faves'
 		}
 	};
 
 	render() {
 		return (
-			<List
+			<Faves
 				data={formatSessionData(this.favouriteFilter(this.props.schedule))}
 				loading={this.props.loading}
 				currentUID={this.props.currentUID}
@@ -100,13 +57,5 @@ const mapStateToProps = state => ({
 	currentUID: state.navigation.currentNavigatorUID,
 	error: state.about.error
 });
-
-FavesContainer.propTypes = {
-	loading: PropTypes.bool,
-	error: PropTypes.string,
-	currentUID: PropTypes.string,
-	schedule: PropTypes.array,
-	faves: PropTypes.object
-};
 
 export default connect(mapStateToProps)(FavesContainer);
